6fca7df5091b9ceaed9182ba33d4b495c902d6aa
Aplet123 <aplet@aplet.me>
2023-07-29 21:16:32 -0400

    update test.js

--- fourth/pyjs/test.js
+++ fourth/pyjs/test.js
@@ -3,10 +3,11 @@ const {PyVal} = require(".");
 const np = PyVal.imp("numpy");
 const plt = PyVal.imp("matplotlib.pyplot");
 
+const mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(np.invert(mat));
+
 const xs = np.arange(0, 20, 0.01);
 const ys = np.sin(xs);
 
 plt.plot(xs, ys);
 plt.show();
-
-setTimeout(() => {}, 5000);
